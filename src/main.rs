ac2eee3c6d693e5e99fb44b4a1799c25bab025bf
Matthew Miller <mattdm@mattdm.org>
2024-06-27 14:38:55 -0400

    it works with shoelace!

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ use tracing::{info, Level};
 
 #[derive(Clone, Routable, Debug, PartialEq, serde::Serialize, serde::Deserialize)]
 enum Route {
-    #[route("/")]
-    Home {},
-    #[route("/blog/:id")]
-    Blog { id: i32 },
+    #[route("/now")]
+    #[redirect("/:.._segments", |_segments: Vec<String>| Route::Now {})]
+    Now {},
+    #[route("/today")]
+    Today {},
 }
 
 fn main() {
@@ -24,51 +25,16 @@ fn App() -> Element {
 }
 
 #[component]
-fn Blog(id: i32) -> Element {
-    rsx! {
-        Link { to: Route::Home {}, "Go to counter" }
-        "Blog post {id}"
-    }
+fn Today() -> Element {
+    todo!("One page at a time...")
 }
 
 #[component]
-fn Home() -> Element {
-    let mut count = use_signal(|| 0);
-    let mut text = use_signal(|| String::from("..."));
-
+fn Now() -> Element {
     rsx! {
-        Link {
-            to: Route::Blog {
-                id: count()
-            },
-            "Go to blog"
-        }
-        div {
-            h1 { "High-Five counter: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
-            button {
-                onclick: move |_| async move {
-                    if let Ok(data) = get_server_data().await {
-                        tracing::info!("Client received: {}", data);
-                        text.set(data.clone());
-                        post_server_data(data).await.unwrap();
-                    }
-                },
-                "Get Server Data"
-            }
-            p { "Server data: {text}"}
+
+        sl-badge {
+            "Hello",
         }
     }
-}
-
-#[server(PostServerData)]
-async fn post_server_data(data: String) -> Result<(), ServerFnError> {
-    info!("Server received: {}", data);
-    Ok(())
-}
-
-#[server(GetServerData)]
-async fn get_server_data() -> Result<String, ServerFnError> {
-    Ok("Hello from the server!".to_string())
 }
