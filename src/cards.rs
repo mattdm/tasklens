9345133ba49099a979413835769e121cc0168fd5
Matthew Miller <mattdm@mattdm.org>
2024-11-09 16:30:56 -0500

    I understand dioxus signals now!

--- src/cards.rs
+++ src/cards.rs
@@ -3,7 +3,17 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn TaskCard() -> Element {
+    let mut edittitle = use_signal(|| false);
+    let mut edittext = use_signal(|| false);
+
+    use_effect(move || println!("edittitle changed to {edittitle:?}"));
+    use_effect(move || println!("edittext changed to {edittext:?}"));
+
     rsx! {
-        div { class: "taskcard", "TK: Task Card" }
+            section { class: "taskcard",
+                h1 { onclick: move |_| edittitle.set(true), contenteditable: if edittitle() { "true" }, "Title TK" },
+                p { onclick: move |_| edittext.set(true), contenteditable: if edittext() { "true" }, "Task text TK" },
+
+        }
     }
 }
