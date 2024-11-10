1ed25d75ca96e76c47b2563a1d35c0ab0385f016
Matthew Miller <mattdm@mattdm.org>
2024-11-10 17:46:07 -0500

    actually a list!

--- src/pages/now.rs
+++ src/pages/now.rs
@@ -5,12 +5,14 @@ use crate::cards::TaskCard;
 
 #[component]
 pub fn Now() -> Element {
-    let now_list = use_signal(|| vec![1, 2, 3]);
+    // "now list only allows three tasks by definition
+    // let now_list = use_signal(|| [i32; 3]);
+    let now_list = use_signal(|| [1, 2, 3]);
 
     rsx! {
         article { class: "taskcolumn",
-            for task in now_list.iter() {
-                TaskCard { task_id: *task }
+            for task in now_list.read() {
+                TaskCard { task_id: task }
             }
         }
     }
