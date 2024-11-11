94ae8f4f045738d70f704cfb3950404b4ce9a516
Matthew Miller <mattdm@mattdm.org>
2024-11-11 18:42:07 -0500

    work in progress to add diesel

--- src/task.rs
+++ src/task.rs
@@ -1,29 +1,44 @@
 #![allow(non_snake_case)]
+use diesel::prelude::*;
 use dioxus::prelude::*;
+use std::env;
 
 #[derive(PartialEq, Props, Clone)]
 pub struct TaskId {
     pub task_id: Option<i32>,
 }
 
-#[derive(PartialEq, Clone)]
+#[derive(PartialEq, Clone, Queryable, Selectable)]
+#[diesel(table_name = crate::schema::tasks)]
+#[diesel(check_for_backend(diesel::sqlite::Sqlite))]
 pub struct Task {
-    pub task_id: i32,
+    pub taskid: i32,
     pub title: String,
     pub detail: String,
-    pub title_html: String,
-    pub detail_html: String,
+    pub titlehtml: String,
+    pub detailhtml: String,
     // TODO: urgency, importance, size, enjoyability, due date, computed priority
 }
 
 pub fn load(id: i32) -> Task {
-    // TODO: load from database!
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let connection = SqliteConnection::establish(&database_url)
+        .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
 
-    Task {
-        task_id: id,
-        title: format!("Task #{id}"),
-        detail: "Details, _details_".to_string(),
-        title_html: format!("Task #{id}"),
-        detail_html: "<p>Details, <em>details</em></p\n".to_string(),
+    results = tasks::table
+        .filter(taskid.eq(id))
+        .limit(1)
+        .select(Task::as_select())
+        .load(connection);
+
+    match results {
+        Ok(result) => result,
+        _ => Task {
+            taskid: id,
+            title: String::new(),
+            detail: String::new(),
+            titlehtml: format!("Task #{id}"),
+            detailhtml: String::new(),
+        },
     }
 }
